562adcbaf0a9757cf6eea55a76d8abb85035ae36
bilal <bilalsuri9898@gmail.com>
2025-03-07 16:41:22 +0500

    functionality with eval

--- eval.js
+++ eval.js
@@ -5,7 +5,6 @@ const deleteOneNumber = document.querySelector(".delete");
 const clearAllNumbers = document.querySelector(".clearAll");
 const calculatorNumbers = document.querySelectorAll(".numbers");
 const calculatorOperators = document.querySelectorAll(".operators");
-const totalFinalAnswer = document.getElementById("final_answer");
 
 let numbersValue;
 let operators;
@@ -14,8 +13,6 @@ calculatorForm.addEventListener("submit", function (event) {
   event.preventDefault();
 });
 
-// calculator functions
-function calculator() {
   calculatorNumbers.forEach((btns) => {
     btns.addEventListener("click", function (event) {
       const btnsValues = Number(event.currentTarget.textContent);
@@ -30,9 +27,7 @@ function calculator() {
       const targetOpBtns = event.currentTarget.textContent;
       operators = targetOpBtns
       inputScreen.value += operators;
-    //   finalAnswer(targetOpBtns);
-    // console.log(operators);
-    
+  
     });
   });
   deleteOneNumber.addEventListener("click", function () {
@@ -44,33 +39,11 @@ function calculator() {
     const target = event.currentTarget;
     target ? (inputScreen.value = "") : null;
   });
-}
 
-// function call
-calculator();
 
 // total function
 function finalAnswer() {
   // with using the eval function
-  console.log(eval(inputScreen.value));
+//   console.log(eval(inputScreen.value));
   inputScreen.value = eval(inputScreen.value)
-
-  // if (inputScreen.value.includes("*")) {
-  //   console.log(numbersValue * numbersValue);
-    
-    
-  // }
-
-  // if (operators === "*") {
-  //   inputScreen.value = Number(numbersValue)
-  // }
-  // if (operators === "/") {
-  //   inputScreen.value = Number(numbersValue / numbersValue)
-  // }
-  // if (operators === "+") {
-  //   inputScreen.value = Number(numbersValue + numbersValue)
-  // }
-  // if (operators === "-") {
-  //   inputScreen.value = Number(numbersValue - numbersValue)
-  // }
 }
